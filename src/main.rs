60b8fdea1525ec7252099a86db67d91b58a94e53
maxfer1221 <maxfer12221@gmail.com>
2022-03-27 22:25:55 -0400

    basic server

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use tiny_http::{Server, Response, Request, Method, Header};
+use std::io::Cursor;
+
+fn main() {
+
+    // tiny_http
+    let server = Server::http("127.0.0.1:8000").unwrap();
+
+    // Serve requests
+    for request in server.incoming_requests() {
+        let route = request.url().split('?').collect::<Vec<&str>>().to_vec()[0];
+
+        let response: Response<Cursor<Vec<u8>>> = match route {
+            "/" => {
+                let id = request.url().split('=').collect::<Vec<&str>>().to_vec()[1];
+
+                let mut res = Response::from_string::<String>(
+                    format!(
+                        "{}\"msg\":\"Hello from server id: {}!\"{}",
+                        '{', id, '}'
+                    )
+                );
+                res.add_header(Header::from_bytes(
+                    &b"Content-Type"[..], &b"application/json"[..]
+                ).unwrap());
+                res
+            },
+            "/ping" => {
+                let mut res = Response::from_string::<String>(String::from("Pong!"));
+                res.add_header(Header::from_bytes(
+                    &b"Content-Type"[..], &b"text/plain"[..]
+                ).unwrap());
+                res
+            },
+            _ => {
+                let mut res = Response::from_string::<String>(String::from("route not found"));
+                res.add_header(Header::from_bytes(
+                    &b"Content-Type"[..], &b"text/plain"[..]
+                ).unwrap());
+                res
+            }
+        };
+
+        // respond
+        request.respond(response).unwrap();
+    }
+
+}
